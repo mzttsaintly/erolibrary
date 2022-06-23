0b73af896964026b730268b99c6074fb7ce07146
Megurim-surface <wy361307103@163.com>
2022-06-23 11:24:53 +0800

    基本框架

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # erolibrary
+为Warfarin-bot 配套用的涩图登记用前后端
+施工中
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+import os
+
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import os
-import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////erolibrary.db'
@@ -38,9 +38,9 @@ db.create_all()  # 创建表
 
 @app.route('/')
 def index():
-    img_list = ImageInformation.query.all()
-    img_stream = return_img_stream(os.path.join(img_list[0].path_name, img_list[0].img_name))
-    return render_template("index.html", img_list=img_list, img_stream=img_stream)
+    img_list = ImageInformation.query.paginate(page=1, per_page=20, error_out=False)
+    img_stream = return_img_stream("static/img/2021-02-25_01.jpg")
+    return render_template("index.html", img_list=img_list.items, img_stream=img_stream)
 
 
 @app.route('/ajax_img', methods=['GET', 'POST'])
@@ -48,9 +48,62 @@ def ajax_img():
     info_img_name = request.form.get('IMG_NAME')
     info_img_path = request.form.get('IMG_PATH')
     new_img = str(info_img_path) + os.path.sep + str(info_img_name)
-    print(new_img)
-    new_img_json = {'data': "data:;base64," + return_img_stream(new_img)}
+    info = select(info_img_name)
+    # print(new_img)
+    new_img_json = {'data': "data:;base64," + return_img_stream(new_img),
+                    'name': info_img_name,
+                    'path': info_img_path,
+                    'ero': info.ero,
+                    'bare': info.bare,
+                    'hentai': info.hentai,
+                    'handsome': info.handsome,
+                    'sex': info.sex}
     return new_img_json
+
+
+@app.route('/change', methods=['GET', 'POST'])
+def change_sql():
+    info_items = request.form.to_dict()
+    info_img_name = request.form.get('img_name')
+    info_img_ero = request.form.get('ero')
+    inf_img_bare = request.form.get('bare')
+    inf_img_hentai = request.form.get('hentai')
+    inf_img_handsome = request.form.get('handsome')
+    inf_img_sex = request.form.get('sex')
+    print(info_items)
+    info_items.pop("img_name")
+    change_info(info_img_name, info_items)
+    # print("info are ", info_img_name, info_img_ero, inf_img_bare, inf_img_hentai, inf_img_handsome, inf_img_sex)
+    return index()
+
+
+def select(img_info: str):
+    """
+    工具函数:
+    根据文件名从数据库中搜索对应的信息
+    :param img_info: 文件名
+    :return: 数据库对象
+    """
+    info = ImageInformation.query.filter_by(img_name=img_info).first()
+    return info
+
+
+def change_info(img_info: str, ch_info_dict: dict):
+    """
+    工具函数:
+    根据文件名从数据库中修改对应的信息
+    :param img_info: 文件名,
+    :param ch_info_dict: 需修改的键与值,
+    :return: None
+    """
+    legal_list = ['ero', 'bare', 'hentai', 'handsome', 'sex']
+    info = ImageInformation.query.filter_by(img_name=img_info).first()
+    for item in ch_info_dict:
+        if item in legal_list:
+            setattr(info, item, ch_info_dict[item])
+        else:
+            pass
+    db.session.commit()
 
 
 def return_img_stream(img_local_path):
@@ -61,7 +114,6 @@ def return_img_stream(img_local_path):
     :return: 图片流
     """
     import base64
-    img_stream = ''
     with open(img_local_path, 'rb') as img_f:
         img_stream = img_f.read()
         img_stream = base64.b64encode(img_stream).decode()
